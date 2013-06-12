5e401fbd1d0115376b51cf4cf33b01379017b4e6
Andrey Petrov <andrey.petrov@shazow.net>
2013-06-12 16:13:28 -0700

    walletnotify hook handler.

---
+++ walletnotify.py
@@ -0,0 +1,22 @@
+import sys
+from pyramid.paster import bootstrap
+
+from coinhooks import api
+
+
+def main(request, tx_id):
+    api.bitcoin.queue_transaction(request.redis, tx_id)
+
+    # TODO: Put this in a looping daemon?
+    api.bitcoin.deque_transaction(request.bitcoin_rpc, request.redis)
+
+
+if __name__ == '__main__':
+    try:
+        ini_path, tx_id = sys.argv[1], sys.argv[2]
+    except IndexError:
+        print 'Usage: %s INI_PATH TX_ID' % sys.argv[0]
+        sys.exit(1)
+
+    env = bootstrap(ini_path)
+    main(env['request'], tx_id)
