5c7e056dba8fa6c679cb413d4913fbcc9198a7a7
Andrey Petrov <andrey.petrov@shazow.net>
2013-06-13 21:42:03 -0700

    request.bitcoin_rpc -> bitcoin

--- bin/walletnotify.py
+++ bin/walletnotify.py
@@ -8,7 +8,7 @@ def main(request, tx_id):
     api.bitcoin.queue_transaction(request.redis, tx_id)
 
     # TODO: Put this in a looping daemon?
-    api.bitcoin.deque_transaction(request.bitcoin_rpc, request.redis)
+    api.bitcoin.deque_transaction(request.bitcoin, request.redis)
 
 
 if __name__ == '__main__':
