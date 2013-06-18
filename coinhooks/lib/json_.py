33f3c24557f06a6939e4ee29c51d123877781b56
Andrey Petrov <andrey.petrov@shazow.net>
2013-06-18 16:53:15 -0700

    FancyEncoder decimal fix.

--- coinhooks/lib/json_.py
+++ coinhooks/lib/json_.py
@@ -10,7 +10,7 @@ class FancyEncoder(json.JSONEncoder):
         if isinstance(obj, datetime.date):
             return time.strftime('%Y-%m-%dT%H:%M:%SZ', obj.utctimetuple())
         elif isinstance(obj, Decimal):
-            return obj.__json__()
+            return float(obj)
         return json.JSONEncoder.default(self, obj)
 
 
